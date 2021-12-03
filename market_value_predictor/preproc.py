67fb6e096882eed281ac3fefa0f3d7ea2f73e6a4
Alexander Pins <alexpins@icloud.com>
2021-12-03 15:44:04 +0000

    Uplaoding my work files

--- market_value_predictor/preproc.py
+++ market_value_predictor/preproc.py
@@ -13,6 +13,8 @@ def manual_encoding(df, column_name):
     df_encode = df_encode.rename(columns=dict(
         zip(init_cols_player_positions, trans_cols_player_positions)))
 
+    df_encode = df_encode.groupby(lambda x: x, axis=1).sum()
+
     df_encode_encoded = df_encode.add_prefix(f"{column_name}_")
 
     df = df.join(df_encode_encoded)
